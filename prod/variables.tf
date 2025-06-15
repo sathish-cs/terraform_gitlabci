586ca19d740593f89e1bf0716dde71b1926f0066
Sathish <83687894+sathish-cs@users.noreply.github.com>
2025-06-15 20:28:58 +0530

    variables for ec2 instance

---
+++ prod/variables.tf
@@ -0,0 +1,49 @@
+variable "web1_ami" {
+  type = string
+}
+
+variable "web1_type" {
+  type = string
+}
+
+variable "web1_subnet" {
+  type = string
+}
+
+variable "web1_key_name" {
+  type = string
+}
+
+variable "web1_name" {
+  type = string
+}
+
+variable "web1_env" {
+  type = string
+}
+
+
+variable "web2_ami" {
+  type = string
+}
+
+variable "web2_type" {
+  type = string
+}
+
+variable "web2_subnet" {
+  type = string
+}
+
+variable "web2_key_name" {
+  type = string
+}
+
+variable "web2_name" {
+  type = string
+}
+
+variable "web2_env" {
+  type = string
+}
+
